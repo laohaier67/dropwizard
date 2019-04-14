5fd1f7aeeda73e49b8950725c0ffb9a687628414
Jochen Schalanda <jochen@schalanda.name>
2019-04-14 19:23:44 +0200

    Add sphinx-copybutton 0.2.5
    https://pypi.org/project/sphinx-copybutton/

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ def get_version():
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.extlinks']
+extensions = ['sphinx.ext.extlinks', 'sphinx_copybutton']
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['ytemplates']
